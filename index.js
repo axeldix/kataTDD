265d2a4af593a0c38f68efe10928b22061fc974b
Matias Cabral <matias.cabral@valtech.com>
2022-09-10 12:50:51 -0300

    teamblue: intengo 1 de resolver con TDD

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const convertArabicToRoman = () => { }
+const numeros_romanos = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" ];
+const numeros_gregorianos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const convertArabicToRoman = (numero_ingresado) => { 
+
+    // switch (numero_ingresado) {
+    //     case 1:
+    //         return "I";
+    //     case 2:
+    //         return "II";
+    //     case 3:
+    //         return "III";
+    //     case 4:
+    //         return "IV";
+    //     case 5:
+    //         return "V";
+    //     case 6:
+    //         return "VI";
+    //     case 7:
+    //         return "VII";
+    //     case 8:
+    //         return "VIII";
+    //     case 9:
+    //         return "IX";
+    //     case 10:
+    //         return "X";
+    //     default:
+    //         break;
+    // }
+
+}
 
 module.exports = convertArabicToRoman
--- index.test.js
+++ index.test.js
@@ -4,5 +4,42 @@ describe("Converts arabic to roman", () => {
   it("should convert 1 to I", () => {
     expect(convertArabicToRoman(1)).toBe("I");
   });
+
+  it("should convert 2 to II", () => {
+    expect(convertArabicToRoman(2)).toBe("II");
+  });
+
+  it("should convert 3 to III", () => {
+    expect(convertArabicToRoman(3)).toBe("III");
+  });
+
+  it("should convert 5 to V", () => {
+    expect(convertArabicToRoman(5)).toBe("V");
+  });
+
+  it("should convert 10 to X", () => {
+    expect(convertArabicToRoman(10)).toBe("X");
+  });
+
+  it("should convert 4 to IV", () => {
+    expect(convertArabicToRoman(4)).toBe("IV");
+  });
+
+  it("should convert 6 to VI", () => {
+    expect(convertArabicToRoman(6)).toBe("VI");
+  });
+
+  it("should convert 7 to VII", () => {
+    expect(convertArabicToRoman(7)).toBe("VII");
+  });
+  
+  it("should convert 8 to VIII", () => {
+    expect(convertArabicToRoman(8)).toBe("VIII");
+  });
+  
+  it("should convert 9 to IX", () => {
+    expect(convertArabicToRoman(9)).toBe("IX");
+  });
+
   
 });
